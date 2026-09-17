d6e13d4ddbb12a3ac5f34eaf6e3ddc5bdb306392
KremeCN <3265494849@qq.com>
2026-09-17 15:19:41 +0800

    fix(ios): spoof H5E location via emval so InitPlayer reaches env.q.
    InitPlayer reads location through emval, not eval/asm_const alone; hook register/property paths and force session reset so H5player fetch can pend.

--- LiteWebTV/Resources/cctv_h5e_fetch_hook.js
+++ LiteWebTV/Resources/cctv_h5e_fetch_hook.js
@@ -8,29 +8,67 @@
             window.__lwtvH5eLastFetch = (window.__lwtvH5eLastFetch || '') + tag;
         }
         var LOCATION_HREF = 'blob:https://tv.cctv.com/5bca710b-9f02-41f0-a9f1-102bbc65192a';
-        function spoofLocation(name) {
-            if (name === 'self.location.host' || name === 'location.host') return '';
-            if (name === 'self.location.protocol' || name === 'location.protocol') return 'blob:';
-            if (name === 'self.location.href' || name === 'location.href') return LOCATION_HREF;
-            return null;
+        var origRegister = typeof __emval_register === 'function' ? __emval_register : null;
+        function tagValue(v, seen) {
+            try {
+                if (!v || typeof v !== 'object') return v;
+                if (seen.indexOf(v) >= 0) return v;
+                seen.push(v);
+                if (typeof v.host === 'string') v.host = '';
+                if (typeof v.protocol === 'string') v.protocol = 'blob:';
+                if (typeof v.href === 'string') v.href = LOCATION_HREF;
+                if (typeof v.location === 'object' && v.location) tagValue(v.location, seen);
+                if (typeof v.self === 'object' && v.self) tagValue(v.self, seen);
+            } catch (e) {}
+            return v;
         }
-        function writeSpoof(text) {
-            var n = lengthBytesUTF8(text) + 1;
-            var ptr = _malloc(n);
-            stringToUTF8(text, ptr, n);
-            return ptr;
+        if (origRegister) {
+            __emval_register = function (value) {
+                var handle = origRegister.apply(this, arguments);
+                try {
+                    var v = value;
+                    if (v && typeof v === 'object' &&
+                        (typeof v.host === 'string' || typeof v.protocol === 'string' ||
+                         typeof v.href === 'string' || typeof v.location === 'object')) {
+                        note('|vg');
+                        tagValue(v, []);
+                    }
+                } catch (e) {}
+                return handle;
+            };
+        }
+        if (typeof __emval_get_property === 'function') {
+            var origProp = __emval_get_property;
+            __emval_get_property = function (obj, prop) {
+                var v = null;
+                try {
+                    var name = requireHandle(prop);
+                    if (name === 'location') note('|vl');
+                    else if (name === 'host') note('|vh');
+                    else if (name === 'protocol') note('|vp');
+                    else if (name === 'href') note('|vr');
+                    v = origProp.apply(this, arguments);
+                    if (name === 'host') v = '';
+                    else if (name === 'protocol') v = 'blob:';
+                    else if (name === 'href') v = LOCATION_HREF;
+                } catch (e) {}
+                return v;
+            };
         }
         if (typeof _emscripten_asm_const_ii === 'function') {
             var origAsm = _emscripten_asm_const_ii;
             _emscripten_asm_const_ii = function (idx, arg) {
                 try {
                     var name = UTF8ToString(arg);
-                    var spoof = spoofLocation(name);
+                    var spoof = null;
+                    if (name === 'self.location.host' || name === 'location.host') { note('|eh'); spoof = ''; }
+                    else if (name === 'self.location.protocol' || name === 'location.protocol') { note('|ep'); spoof = 'blob:'; }
+                    else if (name === 'self.location.href' || name === 'location.href') { note('|er'); spoof = LOCATION_HREF; }
                     if (spoof !== null) {
-                        if (name.indexOf('host') >= 0) note('|eh');
-                        else if (name.indexOf('protocol') >= 0) note('|ep');
-                        else note('|er');
-                        return writeSpoof(spoof);
+                        var n = lengthBytesUTF8(spoof) + 1;
+                        var ptr = _malloc(n);
+                        stringToUTF8(spoof, ptr, n);
+                        return ptr;
                     }
                     if (name && name.indexOf('location') >= 0) note('|ac:' + name);
                 } catch (e) {}
